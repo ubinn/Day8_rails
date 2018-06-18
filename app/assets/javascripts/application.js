544ae80b40650d05349e9fecf38302cca7d77cc5
jane shin <rmhg5140@naver.com>
2018-06-18 06:20:29 +0000

    180618

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,24 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require toastr_rails
 //= require popper
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
+
--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,6 +2,7 @@ class TweetController < ApplicationController
     
     def index
         @contents = Tweet.all
+       #  cookies[:user_name] = "신유빈"
     end
     
     def new
@@ -15,7 +16,8 @@ class TweetController < ApplicationController
         t1.nickname=params[:nickname]
         #t1.time=request.date.to_s
         t1.save
-        redirect_to "/tweet"
+        flash[:success] = "새 글이 등록되었습니다."
+        redirect_to "/tweet/#{t1.id}"
     end
     
     def show
@@ -30,13 +32,16 @@ class TweetController < ApplicationController
         tweet=Tweet.find(params[:id])
         tweet.contents = params[:content]
         tweet.nickname=params[:nickname]
+        tweet.ip_address = request.ip
         tweet.save
-        redirect_to "/tweet"
+        flash[:success] = "글이 수정되었습니다."
+        redirect_to "/tweet/#{tweet.id}"
     end
     
     def destroy
         id=Tweet.find(params[:id])
         id.destroy
+        flash[:error] = "글이 삭제되었습니다."
         redirect_to "/tweet"
     end
 
